471cd2260ede839b6b16735015e98ac08cc24676
JordiBacardit <111888164+JordiBacardit@users.noreply.github.com>
2024-12-19 11:22:42 +0100

    Update codi_contingut.js

--- codi_contingut.js
+++ codi_contingut.js
@@ -1,16 +1,19 @@
-function mostra(enll, idSold){
-    let ll_soldats = document.getElementsByClassName("soldats");
-    if (ll_soldats.length > 0) ll_soldats[0].className = "noVisible";
-
-    document.getElementById(idSold).className = "soldats";
+function mostra(idSold){
+    
+    let llArts = document.querySelectorAll("article");
+    for (let i=0; i<llArts.length; ++i)
+        llArts[i].className = "noVisible";
+    let soldat = document.getElementById(idSold);
+    soldat.className = "";
 }
 
 window.onload = function() {
     let ll_enll = document.querySelectorAll(".enllaç");
     for(let i = 0; i < ll_enll.length; ++i){
         ll_enll[i].onclick = function(){
-            mostra(this, this.getAttribute("funcio_soldats"));
+            mostra(this.getAttribute("data-funcio_soldats"));
         }
     }
-    ll_enll[0].classlist.remove("noVisible");
+    
+    document.querySelector("article").className = "";
 }
